50bead76d9c05b89c0c9088c0f7a9c4268d85f29
luna <lunapadilla.8@gmail.com>
2025-03-08 20:21:54 +0100

    feat: comentario

--- blog-travels-frontend/src/main.jsx
+++ blog-travels-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import "./index.css";
 import App from "./app.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
 // import { AuthProvider } from './contexts/auth-context.jsx';
-
+// comentario 
 createRoot(document.getElementById("root")).render(
   <Router>
     <AuthProvider>
